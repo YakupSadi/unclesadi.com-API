08e787bd77c8be8df8ba4bc6c59c6e038203da58
YakupSadi <sadiyakupsadi@hotmail.com>
2023-12-08 15:43:14 +0300

    All Files Commited

---
+++ db/connect.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+mongoose.set("strictQuery", false)
+
+
+const connectDB = (url) => {
+    return mongoose
+        .connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        .then(() => console.log('Connection is Success'))
+        .catch((err) => console.log(err))
+}
+
+
+module.exports = connectDB
---
+++ middleware/asycn.js
@@ -0,0 +1,15 @@
+const async = (fn) => {
+    return async (req, res, next) => {
+        try 
+        {
+            await fn(req, res, next)
+        }
+        catch (err)
+        {
+            next(err)
+        }
+    }
+}
+
+
+module.exports = async
